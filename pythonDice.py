a80eebf98a7725fda8b3c37c31c21a4e9c665b43
Eniv3n <Eniv3n@gmail.com>
2021-09-21 15:31:51 +0200

    Update pythonDice.py

--- pythonDice.py
+++ pythonDice.py
@@ -1,10 +1,11 @@
 import random
 
+# Defines a function to allow user to roll one standard 6-sided die.
 def rollD6():
   rolled = random.randint(1,6)
   return(rolled)
 
-
+# defines a funtion that uses the previous funtction to roll a D6 4 times.
 def roll4d6():
     rolls = []
     counter = 0
