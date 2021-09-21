689103b8163adeeda151bc943f7e4c003e2a5f79
Eniv3n <Eniv3n@gmail.com>
2021-09-21 15:28:36 +0200

    Update pythonDice.py

--- pythonDice.py
+++ pythonDice.py
@@ -3,4 +3,13 @@ import random
 def rollD6():
   rolled = random.randint(1,6)
   return(rolled)
-  print(rolled)
+
+
+def roll4d6():
+    rolls = []
+    counter = 0
+    while counter < 4:
+        rolled = rollD6()
+        rolls.append(rolled)
+        counter += 1
+    print(rolls)
